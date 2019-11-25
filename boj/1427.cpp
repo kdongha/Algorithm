f8c8c9478607f642001561853ee9d371b8ceb4ee
kdongha <kdongha1597@gmail.com>
2019-11-25 20:55:50 +0900

    solved boj1427

---
+++ boj/1427.cpp
@@ -0,0 +1,20 @@
+//
+// Created by kdongha on 2019/11/25.
+//
+
+#include <bits/stdc++.h>
+
+void solve() {
+    std::string str;
+    std::cin>>str;
+    std::sort(str.begin(),str.end(),std::greater<char>());
+    std::cout<<str;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    solve();
+    return 0;
+}
