8a5f1bd4565f557d9b1c313a188d9bf1a7086907
Dongha <kdongha1597@gmail.com>
2020-02-29 03:34:52 +0900

    solved boj11654

---
+++ boj/11654.cpp
@@ -0,0 +1,22 @@
+//
+// Created by kdongha on 2020/02/29.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char c;
+
+void solve() {
+    cin >> c;
+    cout << 0 + c;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
