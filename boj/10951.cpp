35854749eda883184ba230a5e33a3241df460824
Dongha <kdongha1597@gmail.com>
2020-03-08 17:55:14 +0900

    solved boj10951

---
+++ boj/10951.cpp
@@ -0,0 +1,23 @@
+//
+// Created by kdongha on 2020/03/08.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int A, B;
+
+void solve() {
+    while (cin>>A>>B) {
+        cout << A + B << "\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
